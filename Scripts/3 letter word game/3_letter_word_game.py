943e1f2c1f980a606c9ba866209409847e2e3073
Hegymegi - Jako Tamas <hjtomipls@gmail.com>
2022-10-23 14:53:23 +0200

    created 3 letter word game

---
+++ Scripts/3 letter word game/3_letter_word_game.py
@@ -0,0 +1,18 @@
+import random
+
+words = ['cat', 'bat', 'via', 'new', 'app', 'mow']
+query = random.choice(words)
+print('___')
+
+word = str()
+while word != "exit":
+    word = input()
+    if word == query:
+        print("You correctly guesses the word\n", word.upper())
+        break
+    num = 0
+    for i in range(3):
+        if word[i] in query:
+            num += 1
+
+    print(word, num)
