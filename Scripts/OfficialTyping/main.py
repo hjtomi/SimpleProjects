cc9aa58b61fe8efd2ece4dda02b62fdacb1385ac
Hegymegi - Jako Tamas <hjtomipls@gmail.com>
2023-03-19 17:54:21 +0100

    OfficialTyping start

---
+++ Scripts/OfficialTyping/main.py
@@ -0,0 +1,9 @@
+# The official typing is made to help myself become a quick typer.
+# The app shows a letter on the screen which the user has to press. If the worong button was pressed you get a warning.
+# If the right button is pressed a new letter appears.
+# This project was made entirely with the official finger placement.
+
+from tkinter import *
+from string import ascii_lowercase
+from random import choice
+print(ascii_lowercase)
