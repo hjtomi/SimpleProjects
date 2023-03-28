e1cd43b3fd7e122fbb324167bbcacfde49530283
Hegymegi - Jako Tamas latitude <hjtomipls@gmail.com>
2023-03-28 12:33:20 +0200

    save

--- Scripts/DataScience/pandas_practice.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-df = pd.DataFrame({'A': [1, 2, 3]})
-print(type(df))
---
+++ Scripts/DataScience/practice.py
@@ -0,0 +1,16 @@
+import matplotlib.axes
+import pandas as pd
+import matplotlib.pyplot as plt
+
+jegyek_df = pd.read_excel('TantargyJegyek.xlsx')
+print(jegyek_df)
+print(plt.style.available)
+plt.style.use('ggplot')
+
+ax: matplotlib.axes.Axes
+fig, ax = plt.subplots()
+
+ax.bar(range(6), range(6))
+ax.set(xlim=(0,1000))
+
+plt.show()
