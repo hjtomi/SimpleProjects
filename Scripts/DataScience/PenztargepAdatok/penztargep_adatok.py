593a73f5821363fab0d0e59080a73092b1d3c612
Hegymegi - Jako Tamas <hjtomipls@gmail.com>
2023-03-30 15:40:26 +0200

    grand excel with dates

--- Scripts/DataScience/PenztargepAdatok/penztargep_adatok.py
+++ Scripts/DataScience/PenztargepAdatok/penztargep_adatok.py
@@ -2,9 +2,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 from natsort import natsorted
+import datetime
+
+FIRST_DATE = datetime.date(2021, 1, 1)
+NUMBER_OF_DAYS = 815
 
 
-def rename_files() -> None:
+def rename_files():
     """
     Renames files that have its month written out with letters
     to ones that have numbers as months.
@@ -30,7 +34,7 @@ def create_grand_excel():
     file_names = natsorted(os.listdir('Excelek'))
     dfs = []
 
-    for file_name in os.listdir('Excelek'):
+    for file_name in file_names:
         df = pd.read_excel(f'Excelek/{file_name}', header=4, skipfooter=6)
         df.rename(columns={'Unnamed: 0': 'nap', 'Kártyás fizetés': 'kartyas_fizetes'}, inplace=True)
         dfs.append(df)
@@ -42,6 +46,11 @@ def create_grand_excel():
     grand_df.to_excel('grand-penztargep.xlsx', sheet_name='Munka1')
 
 
-df = pd.read_excel('grand-penztargep.xlsx')
-plt.plot(df.index, df['kartyas_fizetes'])
-plt.show()
+def dates_to_grand():
+    def dates_generator():
+        for i in range(NUMBER_OF_DAYS + 1):
+            yield FIRST_DATE + datetime.timedelta(i)
+
+    df = pd.read_excel('grand-penztargep.xlsx')
+    df['datum'] = pd.Series(list(dates_generator()))
+    df.to_excel('grand-excel-datummal.xlsx')
